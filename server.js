cc9c29d8ec546b04b6d6c915b92e201c93572089
S14mx <sergey.otryshko@gmail.com>
2022-01-08 21:05:56 -0800

    added apis for weather and movies

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const weatherData = require('./data/weather.json');
+const axios = require('axios');
+
 
 const app = express();
 app.use(cors());
@@ -19,33 +20,55 @@ class Forecast {
   }
 }
 
-const truncate = (fullString) => {
-  if (!fullString) return;
+class Movie {
+  constructor(movie) {
+    this.title = movie.original_title;
+    this.overview = movie.overview;
+    this.avgVotes = movie.vote_average;
+    this.totalVotes = movie.vote_count;
+    this.image = movie.poster_path;
+    this.popularity = movie.popularity;
+    this.releaseDate = movie.release_date;
+  }
+}
 
-  fullString = fullString.toString();
-  const dotIndex = fullString.indexOf('.');
-  return dotIndex ? fullString.slice(0, dotIndex + 3) : fullString;
-};
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async (req, res) => {
   const lat = req.query.lat;
   const lon = req.query.lon;
-  const searchQuery = req.query.searchQuery;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&days=7`;
+  const response = await axios.get(url);
 
-  if (!lat || !lon || !searchQuery) {
+  if (!lat || !lon) {
     res.status(400).send('bad request');
   }
 
-  const result = weatherData.find(obj => truncate(obj.lat) === truncate(lat) && truncate(obj.lon) === truncate(lon));
-  if (result) {
-    const weatherArr = result.data.map(day => new Forecast(day, lat, lon));
+  if (response) {
+    const weatherArr = response.data.data.map(day => new Forecast(day, lat, lon));
     res.status(200).send(weatherArr);
 
   } else {
-
     res.status(204).send('city not found');
   }
 });
 
+app.get('/movies', async (req, res) => {
+  const searchQuery = req.query.searchQuery;
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+  const response = await axios.get(url);
+
+  if (!searchQuery) {
+    res.status(400).send('bad request');
+  }
+
+  if (response) {
+    const moviesArr = response.data.results.map(movie => new Movie(movie));
+    res.status(200).send(moviesArr);
+
+  } else {
+    res.status(204).send('movie not found');
+  }
+
+});
 
 app.listen(PORT, () => console.log(PORT));
