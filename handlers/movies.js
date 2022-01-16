41af952e145432c0eeefdeb82cd6a46f3618ee96
S14mx <sergey.otryshko@gmail.com>
2022-01-15 17:05:46 -0800

    implemented local cache for weather and movies

--- handlers/movies.js
+++ handlers/movies.js
@@ -2,18 +2,31 @@
 
 const axios = require('axios');
 const Movie = require('../classes/Movie.js');
+const cache = require('../data/cache.js');
 
 const getMovies = async (req, res) => {
   try {
     const searchQuery = req.query.searchQuery;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-    const response = await axios.get(url);
+    console.log('line 11', cache);
 
-    if (response) {
-      const moviesArr = response.data.results.map(movie => new Movie(movie));
-      res.status(200).send(moviesArr);
+    if (cache[`${searchQuery}Movies`] && (Date.now() - cache[`${searchQuery}Movies`].timeStamp) < (1000 * 60 * 60 * 24 * 3)) {
+      console.log('cache hit');
+      // console.log('movies array line 16', cache[`${searchQuery}Movies`].movieData);
+      res.send(cache[`${searchQuery}Movies`].movieData);
+    } else {
+      const response = await axios.get(url);
+      if (response) {
+        const moviesArr = response.data.results.map(movie => new Movie(movie));
+        console.log('cache miss');
+        cache[`${searchQuery}Movies`] = {
+          movieData: moviesArr,
+          timeStamp: Date.now()
+        };
+        console.log('line 28', cache);
+        res.status(200).send(moviesArr);
+      }
     }
-
   } catch (err) {
     res.send(err.response);
   }
--- handlers/weather.js
+++ handlers/weather.js
@@ -2,19 +2,33 @@
 
 const axios = require('axios');
 const Forecast = require('../classes/Forecast.js');
+const cache = require('../data/cache.js');
 
 const getWeather = async (req, res) => {
+  console.log('Im running');
   try {
     const lat = req.query.lat;
     const lon = req.query.lon;
+    const WEATHER_CACHE_KEY = `${lat}${lon}Weather`;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&days=7`;
-    const response = await axios.get(url);
-
-    if (response) {
-      const weatherArr = response.data.data.map(day => new Forecast(day, lat, lon));
-      res.status(200).send(weatherArr);
+    console.log('from weather', cache);
+    if (cache[WEATHER_CACHE_KEY] && (Date.now() - cache[WEATHER_CACHE_KEY].timeStamp) < (1000 * 60 * 60 * 3)) {
+      console.log('weather cache hit');
+      res.send(cache[WEATHER_CACHE_KEY].weatherData);
+    } else {
+      const response = await axios.get(url);
+      if (response) {
+        const weatherArr = response.data.data.map(day => new Forecast(day, lat, lon));
+        console.log('weather cache miss');
+        cache[WEATHER_CACHE_KEY] = {
+          weatherData: weatherArr,
+          timeStamp: Date.now()
+        };
+        res.status(200).send(weatherArr);
+      }
     }
   } catch (err) {
+    console.log(err.response);
     res.send(err.response);
   }
 };
