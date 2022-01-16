e1fd50ec1e509503f8c64a9c34c6ec7dbdf03359
S14mx <sergey.otryshko@gmail.com>
2022-01-15 17:50:53 -0800

    fixed cross refference error

--- handlers/movies.js
+++ handlers/movies.js
@@ -8,27 +8,24 @@ const getMovies = async (req, res) => {
   try {
     const searchQuery = req.query.searchQuery;
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-    console.log('line 11', cache);
 
     if (cache[`${searchQuery}Movies`] && (Date.now() - cache[`${searchQuery}Movies`].timeStamp) < (1000 * 60 * 60 * 24 * 3)) {
-      console.log('cache hit');
-      // console.log('movies array line 16', cache[`${searchQuery}Movies`].movieData);
+      console.log('movies cache hit');
       res.send(cache[`${searchQuery}Movies`].movieData);
     } else {
       const response = await axios.get(url);
       if (response) {
         const moviesArr = response.data.results.map(movie => new Movie(movie));
-        console.log('cache miss');
+        console.log('movies cache miss');
         cache[`${searchQuery}Movies`] = {
           movieData: moviesArr,
           timeStamp: Date.now()
         };
-        console.log('line 28', cache);
         res.status(200).send(moviesArr);
       }
     }
   } catch (err) {
-    res.send(err.response);
+    res.send('error');
   }
 };
 
--- handlers/weather.js
+++ handlers/weather.js
@@ -5,13 +5,12 @@ const Forecast = require('../classes/Forecast.js');
 const cache = require('../data/cache.js');
 
 const getWeather = async (req, res) => {
-  console.log('Im running');
   try {
     const lat = req.query.lat;
     const lon = req.query.lon;
     const WEATHER_CACHE_KEY = `${lat}${lon}Weather`;
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}&days=7`;
-    console.log('from weather', cache);
+
     if (cache[WEATHER_CACHE_KEY] && (Date.now() - cache[WEATHER_CACHE_KEY].timeStamp) < (1000 * 60 * 60 * 3)) {
       console.log('weather cache hit');
       res.send(cache[WEATHER_CACHE_KEY].weatherData);
@@ -28,8 +27,7 @@ const getWeather = async (req, res) => {
       }
     }
   } catch (err) {
-    console.log(err.response);
-    res.send(err.response);
+    res.send('error');
   }
 };
 
